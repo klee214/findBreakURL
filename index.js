fd2c1acaf9bd7415aa251a1b2118f46dd7c089d8
kiminlee <kiminlee0505@gmail.com>
2020-09-16 14:53:16 -0400

    before deploying

--- index.js
+++ index.js
@@ -1,57 +1,85 @@
-const axios = require('axios');
-const fs = require('fs');
-const validator = require('validator');
-const yargs = require('yargs')
-const chalk = require('chalk');
-const { argv } = require('process');
+const axios = require("axios");
+const fs = require("fs");
+const validator = require("validator");
+const yargs = require("yargs");
+const chalk = require("chalk");
 
-yargs.usage('Usage: node $0 <command> [options] <optionalFilename>')
-.command('test', 'Test to find any broken URL')
-.example('node $0 test -f foo.html',' Test if there is any broken URL in the html file')
-.example('node $0 test -f -a',' Test broken URL in the html files in current dir')
-.alias('f', 'file')
-.alias('a', 'all')
-.demandOption(['f'])
-.describe('f','Load a specified HTML file')
-.describe('a','Load all HTML files in the current dir')
-.version()
-.alias('v','version')
-.help()
-.alias('h', 'help')
-.demandCommand()
-.argv
+yargs
+  .usage("Usage: node $0 <command> [options] <optionalFilename>")
+  .command("test", "Test to find any broken URL")
+  .example(
+    "node $0 test -f foo.html",
+    " Test if there is any broken URL in the html file"
+  )
+  .example(
+    "node $0 test -f -a",
+    " Test broken URL in the html files in current dir"
+  )
+  .alias("f", "file")
+  .alias("a", "all")
+  .demandOption(["f"])
+  .describe("f", "Load a specified HTML file")
+  .describe("a", "Load all HTML files in the current dir")
+  .version()
+  .alias("v", "version")
+  .help()
+  .alias("h", "help")
+  .demandCommand().argv;
 
-console.log(argv)
+let fileData = null;
+let files = [];
 
-const fileData = fs.readFileSync(`${__dirname}\\index.html`,{encoding:"utf-8"});
-
-const regex = /(https|http):\/\/.*["]/g;
-const findURL = fileData.match(regex);
-
-const fetchFunction = async (url)=>{
-
-    try {
-        const response = await axios.get(url);
-        console.log(response.status)
-        
-    } catch (error) {
-        if(error.response){
-            console.log(error.response.status)   
-        }
-
-        console.log("ENOTFOUND");
-    }
+// decide the option either -f or -a
+if (yargs.argv.a || typeof yargs.argv.f !== "string") {
+  const tmpFiles = fs.readdirSync(__dirname, { encoding: "utf-8" });
+  files = tmpFiles.filter((file) => {
+    return file.toLowerCase().endsWith(".html");
+  });
+} else if (typeof yargs.argv.f === "string") {
+  files.push(yargs.argv.f);
 }
 
-findURL.forEach((url, index) => {
+// map through the array fetch each html file
+files.map((file) => {
+  fileData = fs.readFileSync(`${__dirname}\\${file}`, {
+    encoding: "utf-8",
+  });
+
+  const regex = /(https:\/\/|http:\/\/)[^"']+"/g;
+  const findURL = fileData.match(regex);
+
+  const fetchFunction = async (url) => {
+    try {
+      const response = await axios.get(url);
+      console.log(
+        chalk.black.bgGreen.bold(
+          "In " + file + " file, the URL: " + url + " is success: "
+        )
+      );
+      console.log(chalk.green.underline.bold("STATUS: " + response.status));
+    } catch (error) {
+      if (error.response) {
+        console.log(
+          chalk.black.bgYellow.bold(
+            "In " + file + " file, the URL: " + url + " has not been found: "
+          )
+        );
+        return console.log(
+          chalk.yellow.underline.bold("STATUS: " + error.response.status)
+        );
+      }
+      chalk.white.bgRed.bold;
+      console.log(
+        chalk.white.bgRed.bold(
+          "In " + file + " file, the URL: " + url + " has an error: "
+        )
+      );
+      console.log(chalk.red.underline.bold("ENOTFOUND"));
+    }
+  };
+
+  findURL.forEach((url, index) => {
     findURL[index] = url.slice(0, url.lastIndexOf('"'));
-    fetchFunction(findURL[index]);
+    if (validator.isURL(findURL[index])) fetchFunction(findURL[index]);
+  });
 });
-
-
-
-
-
-
-
-
